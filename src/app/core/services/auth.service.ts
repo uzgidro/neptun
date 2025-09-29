603cb88c06ce807389c8cf05974db3183b813485
Timur <toirovtimurmalik@gmail.com>
2025-09-29 11:36:26 +0500

    send post login request save access token

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+export interface AuthResponse {
+    access_token: string;
+    refresh_token: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    // Замените URL на ваш реальный эндпоинт API
+    private apiUrl = 'https://prime.speedwagon.uz/auth/sign-in';
+
+    constructor(private http: HttpClient) {}
+
+    login(name: string, password: string): Observable<AuthResponse> {
+        return this.http.post<AuthResponse>(this.apiUrl, { name, password });
+    }
+}
