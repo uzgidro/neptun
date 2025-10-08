72f55f1127176af87d89bf9665ba860ad37f8941
Timur <toirovtimurmalik@gmail.com>
2025-10-08 16:35:56 +0500

    pdf-viewer

--- src/app/pages/document-viewer/document-viewer.component.ts
+++ src/app/pages/document-viewer/document-viewer.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Tab, TabList, Tabs } from 'primeng/tabs';
 import { FileUpload, FileUploadEvent } from 'primeng/fileupload';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
     selector: 'app-document-viewer',
     standalone: true,
-    imports: [Tab, TabList, Tabs, FileUpload],
+    imports: [Tab, TabList, Tabs, FileUpload, PdfViewerModule],
     templateUrl: './document-viewer.component.html',
-    styleUrls: ['./document-viewer.component.scss'] // ✅ здесь исправлено
+    styleUrls: ['./document-viewer.component.scss']
 })
 class DocumentViewerComponent {
+    pdfSrc = 'assets/example.pdf';
     onUpload($event: FileUploadEvent) {
         // обработка загрузки
     }
