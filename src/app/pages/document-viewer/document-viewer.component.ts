3bb4b1e032523d3eaccdba0cf370cbe8c8428d22
suxa56 <suxa5600@gmail.com>
2025-10-10 20:40:30 +0500

    show files

--- src/app/pages/document-viewer/document-viewer.component.ts
+++ src/app/pages/document-viewer/document-viewer.component.ts
@@ -3,6 +3,8 @@ import { Tab, TabList, Tabs } from 'primeng/tabs';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
+import { ApiService } from '@/core/services/api.service';
+import { LatestFiles } from '@/core/interfaces/latest-files';
 
 @Component({
     selector: 'app-document-viewer',
@@ -14,47 +16,67 @@ import { Subject, takeUntil } from 'rxjs';
 export class DocumentViewerComponent implements OnInit, OnDestroy {
     private route = inject(ActivatedRoute);
     private router = inject(Router);
+    private apiService = inject(ApiService);
     private destroy$ = new Subject<void>();
 
     showTabs = false;
     activeIndex = 0;
 
-    pdfSources: string[] = [
-        'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf', // 0: Норин
-        'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf', // 1: Чаткал (замените на другой)
-        'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf', // 2: ЮФК (замените на другой)
-        'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf' // 3: Оксув (замените на другой)
-    ];
+    files: LatestFiles[] = [];
+
+    fileUrl = '';
 
     ngOnInit() {
-        this.route.queryParams.pipe(
-            takeUntil(this.destroy$)
-        ).subscribe((params) => {
+        this.apiService.getLatestFiles().subscribe({
+            next: (data) => {
+                this.files = data;
+            }
+        });
+        this.route.queryParams.pipe(takeUntil(this.destroy$)).subscribe((params) => {
             const isConstruction = params['type'] === 'construction';
             this.showTabs = isConstruction;
 
             if (isConstruction) {
+                let tabIndex = 0;
                 if (params['tabIndex']) {
-                    const tabIndex = parseInt(params['tabIndex'], 10);
-                    if (!isNaN(tabIndex) && tabIndex >= 0 && tabIndex < this.pdfSources.length) {
+                    tabIndex = parseInt(params['tabIndex'], 10);
+                    if (!isNaN(tabIndex) && tabIndex >= 0 && tabIndex < this.files.length) {
                         this.activeIndex = tabIndex;
                     }
                 } else {
+                    tabIndex = 0;
                     this.router.navigate([], {
                         queryParams: { tabIndex: 0 },
                         queryParamsHandling: 'merge',
                         replaceUrl: true
                     });
                 }
+
+                switch (tabIndex) {
+                    case 0:
+                        this.fileUrl = this.files.filter((value) => value.category_name.includes('norin'))[0].url;
+                        break;
+                    case 1:
+                        this.fileUrl = this.files.filter((value) => value.category_name.includes('chotqol'))[0].url;
+                        break;
+                    case 2:
+                        this.fileUrl = this.files.filter((value) => value.category_name.includes('ufk'))[0].url;
+                        break;
+                    case 3:
+                        this.fileUrl = this.files.filter((value) => value.category_name.includes('oqsuv'))[0].url;
+                        break;
+                }
+            }
+
+            if (params['type'] === 'production') {
+                this.fileUrl = this.files.filter((value) => value.category_name.includes('production'))[0].url;
+            } else if (params['type'] === 'shutdown') {
+                this.fileUrl = this.files.filter((value) => value.category_name.includes('shutdown'))[0].url;
             }
         });
     }
 
-    get currentPdfSrc(): string {
-        return this.pdfSources[this.activeIndex];
-    }
-
-    onTabChange(index: number) {
+    onTabChange(index: number | string) {
         this.router.navigate([], { queryParams: { tabIndex: index }, queryParamsHandling: 'merge' });
     }
 
