37a3bf2eed4a689a702e170d7fd2ca8b38a7dc28
suxa56 <suxa5600@gmail.com>
2025-11-11 11:55:08 +0500

    specify incidents response

--- src/app/core/interfaces/incidents.ts
+++ src/app/core/interfaces/incidents.ts
@@ -3,3 +3,14 @@ export interface IncidentPayload {
     incident_time?: string;
     description?: string;
 }
+
+export interface IncidentResponse {
+    id: number;
+    incident_date: string;
+    description: string | null;
+    created_at: string;
+    organization_id: number;
+    organization: string;
+    created_by_user_id: number;
+    created_by_user: string;
+}
--- src/app/core/services/incident.service.ts
+++ src/app/core/services/incident.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService, BASE_URL } from '@/core/services/api.service';
 import { Observable } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
-import { IncidentPayload } from '@/core/interfaces/incidents';
+import { IncidentPayload, IncidentResponse } from '@/core/interfaces/incidents';
 
 const INCIDENTS = '/incidents';
 
@@ -10,13 +10,12 @@ const INCIDENTS = '/incidents';
     providedIn: 'root'
 })
 export class IncidentService extends ApiService {
-    // TODO(): Specify type for response
-    getIncidents(date?: Date): Observable<any> {
+    getIncidents(date?: Date): Observable<IncidentResponse[]> {
         const params = new HttpParams();
         if (date) {
             params.set('date', date.toISOString());
         }
-        return this.http.get(BASE_URL + INCIDENTS, { params: params });
+        return this.http.get<IncidentResponse[]>(BASE_URL + INCIDENTS, { params: params });
     }
 
     addIncident(payload: IncidentPayload): Observable<any> {
