8e65079bf8005285617e4cc06836c4a9747efcbe
suxa56 <suxa5600@gmail.com>
2025-11-20 18:18:10 +0500

    use new api for ges

--- src/app/core/interfaces/organizations.ts
+++ src/app/core/interfaces/organizations.ts
@@ -1,8 +1,14 @@
+import { Contact } from '@/core/interfaces/contact';
+import { AscueMetricsDto } from '@/core/interfaces/ascue';
+
 export interface Organization {
     id: number;
     name: string;
     parent_organization_id?: number | null;
     parent_organization?: string | null;
-    types: string[];
+    types?: string[];
     items?: Organization[];
+    current_discharge?: number | null;
+    contacts: Contact[];
+    ascue_metrics?: AscueMetricsDto;
 }
