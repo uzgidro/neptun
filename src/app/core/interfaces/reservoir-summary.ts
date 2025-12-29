22e575f0f738915f2ee6f4474b6d0f107fc6697c
suxa56 <suxa5600@gmail.com>
2025-12-29 11:58:25 +0500

    remove redundant fields

--- src/app/core/interfaces/reservoir-summary.ts
+++ src/app/core/interfaces/reservoir-summary.ts
@@ -7,19 +7,6 @@ export interface ValueResponse {
 }
 
 export interface ReservoirSummaryResponse {
-    reservoir_name: any;
-    volume_today: any;
-    volume_yesterday: any;
-    volume_difference: any;
-    volume_1_year_ago: any;
-    volume_2_years_ago: any;
-    level_today: any;
-    level_yesterday: any;
-    level_difference: any;
-    level_1_year_ago: any;
-    level_2_years_ago: any;
-    outflow: any;
-    inflow: any;
     organization_id: number | null;
     organization_name: string | null;
     income: ValueResponse;
