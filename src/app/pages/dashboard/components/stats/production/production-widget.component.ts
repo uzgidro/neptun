e05f34b4db3a9721328f19e66fb802c6f372ecab
suxa56 <suxa5600@gmail.com>
2026-01-08 19:41:08 +0500

    display production on dashboard

--- src/app/pages/dashboard/components/stats/production/production-widget.component.ts
+++ src/app/pages/dashboard/components/stats/production/production-widget.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DashboardService } from '@/core/services/dashboard.service';
+import { DashboardResponse } from '@/core/interfaces/ges-production';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-production',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './production-widget.component.html',
     styleUrl: './production-widget.component.scss'
 })
-export class ProductionWidget {}
+export class ProductionWidget implements OnInit {
+    private dashboardService = inject(DashboardService);
+
+    data: DashboardResponse | null = null;
+    isLoading = false;
+
+    ngOnInit() {
+        this.loadData();
+    }
+
+    private loadData() {
+        this.isLoading = true;
+        this.dashboardService.getGESProduction().subscribe({
+            next: (response) => {
+                this.data = response;
+                this.isLoading = false;
+            },
+            error: (err) => {
+                console.error('Failed to load production data', err);
+                this.isLoading = false;
+            }
+        });
+    }
+}
